617ebfe83d672a7832afe450cb19ea1be9aa7ce6
figsforpigs <132413881+figsforpigs@users.noreply.github.com>
2023-05-18 15:02:57 +1000

    Update import.py

--- import.py
+++ import.py
@@ -1,6 +1,7 @@
 from import datetime*
 from import asyncio*
-from math import *
+from import math*
+
 def remove_class(element, class_name):
     element.element.classList.remove(class_name)
                   
