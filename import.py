e7b0a1fe5b5da305ec14f652838c66681e1cce18
figsforpigs <132413881+figsforpigs@users.noreply.github.com>
2023-05-18 13:13:59 +1000

    Add files via upload

---
+++ import.py
@@ -0,0 +1,8 @@
+from import datetime*
+from import asyncio
+
+def remove_class(element, class_name):
+    element.element.classList.remove(class_name)
+                  
+def add_class(element, class_name):
+    element.element.classList.add(class_name)
