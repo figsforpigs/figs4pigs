999dab78a0a258c8a0a9fe6bec5870df30af9573
figsforpigs <132413881+figsforpigs@users.noreply.github.com>
2023-05-19 13:57:58 +1000

    Update import.py

--- import.py
+++ import.py
@@ -1,7 +1,10 @@
 from import datetime*
 from import asyncio*
 from import math*
+from import time*
 
+import time
+time.sleep(10)
 def remove_class(element, class_name):
     element.element.classList.remove(class_name)
                   
